af73f38ea44bcbd96960fe1b18764b9e4cf9d8df
JYHsu <hsujoyao@gmail.com>
2021-06-14 00:35:17 +0800

    [feat] Add expenses chart bar

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import ExpensesList from "./ExpensesList";
 import ExpenseFilter from "./ExpenseFilter";
 import "./Expenses.css";
 import { useState } from "react";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState(new Date().getFullYear().toString());
@@ -11,12 +12,14 @@ const Expenses = (props) => {
     };
 
     const filteredExpenses = props.items.filter((expense) => {
+        console.log(expense);
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
     return (
         <div className="expenses">
             <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpensesList items={filteredExpenses}></ExpensesList>
         </div>
     );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -48,7 +48,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: userInput.enteredTitle,
-            amount: userInput.enteredAmount,
+            amount: +userInput.enteredAmount,
             date: new Date(userInput.enteredDate),
         };
         props.onSaveExpenseData(expenseData);
