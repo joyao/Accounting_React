37fac44880846a903ca5e3f81469dcfeeaafecb5
JYHsu <hsujoyao@gmail.com>
2021-06-13 16:22:13 +0800

    [feat] Add new expense form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    const ChangeTitleHandler = () => {
+        setTitle("Updated");
+        alert("click!");
+    };
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount.toString()}</div>
+                <h2>{title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={ChangeTitleHandler}>Change Title</button>
         </Card>
     );
 };
