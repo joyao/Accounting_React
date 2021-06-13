825f2b5d94574ec0796d483ef825c61eb6890a74
JYHsu <hsujoyao@gmail.com>
2021-06-13 12:16:50 +0800

    [fix] Change date showing from zh-TW to en-US

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,8 +1,8 @@
 import "./ExpenseDate.css";
 
 const ExpenseDate = (props) => {
-    const month = props.date.toLocaleString("zh-TW", { month: "long" });
-    const day = props.date.toLocaleString("zh-TW", { day: "2-digit" });
+    const month = props.date.toLocaleString("en-US", { month: "long" });
+    const day = props.date.toLocaleString("en-US", { day: "2-digit" });
     const year = props.date.getFullYear();
     return (
         <div className="expense-date">
