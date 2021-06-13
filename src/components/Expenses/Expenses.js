22605e5c020506ea44a62a7cc65226b54168d724
JYHsu <hsujoyao@gmail.com>
2021-06-13 17:41:37 +0800

    [feat] Add year filter dropdown and state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,18 @@
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 import "./Expenses.css";
+import { useState } from "react";
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState("2020");
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
     return (
         <div className="expenses">
+            <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             {props.items.map((i) => {
                 return <ExpenseItem title={i.title} date={i.date} amount={i.amount} />;
             })}
